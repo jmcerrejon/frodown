a557c7545ce6b87e6867703bc64dab92e996becf
Jose Cerrejon <jmcerrejon@sioxfer.com>
2024-02-16 11:05:10 +0100

    feat: Add sidebar. Refactor code

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import datetime
 from textual import on
 from textual.app import App, ComposeResult
 from textual.binding import Binding
-from textual.widgets import Header, Footer, Input, Label, Select, TextArea, Button
+from textual.reactive import reactive
+from textual.containers import Container
+from textual.widgets import (
+    Static,
+    Header,
+    Footer,
+    Input,
+    Label,
+    Select,
+    TextArea,
+    Button,
+)
 
 AUTHOR = "Jose Cerrejon"
 CATEGORIES = """General
@@ -26,32 +37,13 @@ Intro text here
 """
 
 
-class Frodown(App):
+class Frodown(App[None]):
+    CSS_PATH = "main.tcss"
     BINDINGS = [
-        Binding(key="ctrl+t", action="toggle_dark", description="Toggle light mode"),
-        Binding(key="ctrl+q", action="quit", description="Quit the app"),
+        Binding(key="ctrl+s", action="toggle_sidebar", description="Cheat Sheet"),
+        Binding(key="ctrl+q", action="quit", description="Quit the App"),
+        Binding(key="ctrl+t", action="toggle_dark", description="Toggle Light Mode"),
     ]
-
-    CSS = """
-    Input.-valid {
-        border: tall;
-    }
-    Input.-valid:focus {
-        border: tall $success;
-    }
-    Input {
-        margin: 1 1;
-    }
-    Label {
-        margin: 1 2;
-    }
-    Select {
-        border: tall;
-    }
-    Button {
-        margin: 1 2;
-    }
-    """
 
     _title: Input
     _author: Input
@@ -60,29 +52,45 @@ class Frodown(App):
     _tags: Input
     _textarea: TextArea
 
-    def compose(self) -> ComposeResult:
-        self._title = Input(placeholder="Title of the article")
-        self._author = Input(placeholder="Author", value=AUTHOR)
-        self._date = Input(placeholder="Date", value=datetime.date.today().isoformat())
-        self._category = Select(((line, line) for line in CATEGORIES), value="General")
-        self._tags = Input(placeholder="Tags separeted with commas", valid_empty=True)
-        self._textarea = TextArea(text=DEFAULT_TEXTAREA, language="markdown")
+    show_sidebar = reactive(False)
 
-        yield Header()
-        yield Label("Title")
-        yield self._title
-        yield Label("Author")
-        yield self._author
-        yield Label("Date")
-        yield self._date
-        yield Label("Category")
-        yield self._category
-        yield Label("Tags")
-        yield self._tags
-        yield Label("Article")
-        yield self._textarea
-        yield Button("Save", variant="primary")
-        yield Footer()
+    def compose(self) -> ComposeResult:
+        self._title = Input(id="title", placeholder="Title of the article")
+        self._author = Input(id="author", placeholder="Author", value=AUTHOR)
+        self._date = Input(
+            id="date", placeholder="Date", value=datetime.date.today().isoformat()
+        )
+        self._category = Select(
+            ((line, line) for line in CATEGORIES), id="category", value="General"
+        )
+        self._tags = Input(
+            id="tags", placeholder="Tags separeted with commas", valid_empty=True
+        )
+        self._textarea = TextArea(
+            id="textarea", text=DEFAULT_TEXTAREA, language="markdown"
+        )
+
+        yield Container(
+            Sidebar(classes="-hidden"),
+            Header(show_clock=True),
+            Label("Title"),
+            self._title,
+            Label("Author"),
+            self._author,
+            Label("Date"),
+            self._date,
+            Label("Category"),
+            self._category,
+            Label("Tags"),
+            self._tags,
+            Label("Article"),
+            self._textarea,
+            Button("Save", variant="primary"),
+            Footer(),
+        )
+
+    def action_toggle_dark(self) -> None:
+        self.dark = not self.dark
 
     @on(Select.Changed)
     def select_changed(self, event: Select.Changed) -> None:
@@ -109,6 +117,65 @@ tags:
             file.write(self._textarea.text)
 
         self.exit(message=f"Article {self._title.value} saved as {filename}!")
+
+    def action_toggle_sidebar(self) -> None:
+        sidebar = self.query_one(Sidebar)
+        self.set_focus(None)
+        if sidebar.has_class("-hidden"):
+            sidebar.remove_class("-hidden")
+        else:
+            if sidebar.query("*:focus"):
+                self.screen.set_focus(None)
+            sidebar.add_class("-hidden")
+
+
+class Title(Static):
+    pass
+
+
+class OptionGroup(Container):
+    pass
+
+
+class Message(Static):
+    pass
+
+
+class Sidebar(Container):
+    def compose(self) -> ComposeResult:
+        yield Title("Cheat Sheet")
+        yield OptionGroup(
+            Message(
+                """::: important Custom important
+...
+:::
+__
+::: info Custom info
+...
+:::
+__
+::: note Custom note
+...
+:::
+__
+::: tip Custom tip
+...
+:::
+__
+::: warning Custom warning
+...
+:::
+__
+::: caution Custom caution
+...
+:::
+__
+Use == == to mark. ==highlighted==
+
+NOTE: My intention is to add a table, so you can select the code and then, the code will be copied to the Textarea.
+"""
+            )
+        )
 
 
 app = Frodown()
